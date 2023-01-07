72bcda0f45c8a67711a62f6f40092cd8f765186a
Henri C <henri.casselle@gmail.com>
2023-01-07 18:36:59 +0100

    orbitControl for debug

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import * as THREE from 'three'
 import HumanHarverstScene from './HumanHarverstScene'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -13,6 +16,20 @@ const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
 
 const scene = new HumanHarverstScene(mainCamera)
 scene.initialize()
+
+const orbitControls = new OrbitControls(mainCamera, renderer.domElement);
+orbitControls.mouseButtons = {
+	MIDDLE: THREE.MOUSE.ROTATE,
+	RIGHT: THREE.MOUSE.PAN,
+	LEFT:THREE.MOUSE.LEFT
+}
+orbitControls.enableDamping = true
+orbitControls.enablePan = true
+orbitControls.minDistance = 5
+orbitControls.maxDistance = 60
+orbitControls.maxPolarAngle = Math.PI / 2 - 0.05 // prevent camera below ground
+orbitControls.minPolarAngle = Math.PI / 4        // prevent top down view
+orbitControls.update();
 
 function tick()
 {
